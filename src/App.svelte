267932d4c2ac1b2e0f36f9ae0748e077585ab29c
tb1995 <tb1995@gmail.com>
2022-02-27 21:32:21 +0500

    Making the library

--- src/App.svelte
+++ src/App.svelte
@@ -5,9 +5,39 @@ import Info_1 from "./Components/Info-1.svelte";
 import GridThreeColumns from "./Components/Product-Grid/Grid-Three-Columns/Grid-Three-Columns.svelte";
 import { Tabs, TabList, TabPanel, Tab } from './Components/Tabs/tabs';
 import ResponsiveGrid from "./Components/Product-Grid/Responsive-Grid.svelte";
+import HeroFullscreen from "./Components/Hero-Fullscreen.svelte";
 
 
 	export let name;
+	const imgDirectory = "/img/component-images/"
+	export let componentArray = {
+		heroes: [
+			{
+			title: "Hero Fullscreen",
+			description: "Hero with 100vh and with a CTA",
+			url: "",
+			imageUrl: imgDirectory + "hero-fullscreen.jpg",
+			dependencies: [],
+			listOfProps: []
+			}
+		],
+		grids: [
+				{
+			title: "Responsive Grid",
+			description: "Specify the number of columns and item width via the props",
+			url: "",
+			imageUrl: imgDirectory + "hero-fullscreen.jpg",
+			dependencies: [],
+			listOfProps: []
+			}
+		],
+		tabs: [],
+		infoSection: [],
+		navbars: [],
+		footers: [],
+		galleries: [],
+		forms: []
+	}
 	export let array = [
     {
         title: "Here is a title",
@@ -46,6 +76,7 @@ import ResponsiveGrid from "./Components/Product-Grid/Responsive-Grid.svelte";
 let fourColumns = 4;
 </script>
 
+<HeroFullscreen />
 <HeroHalfscreen></HeroHalfscreen>
 <ResponsiveGrid
 		array = {array}
